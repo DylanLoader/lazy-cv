4ed0e665c6c5067d60781ad228d9069be5731407
Dylan Loader <dylanloader@gmail.com>
2023-11-07 23:36:51 -0700

    Minor changes to max_tokens, still need to fix output.

--- src/app.py
+++ src/app.py
@@ -48,68 +48,74 @@ with st.spinner(text="Generating Cover Letter"):
         #     # Save the user data to the json
         #     pass
         # Prompt generation
-        pre_prompt = "You are an AI cover letter writing assistant and your task to write a persuasive cover letter using the following information: "
+        pre_prompt = "You are an AI cover letter writing assistant and your task to write a persuasive cover letter using the following information: \n"
         # Prompts from form
         # Candidate Information
-        prompt = pre_prompt + f"The candidate's name is {applicant_fname} {applicant_lname}. "
-        prompt += f"The candidate's experience is: {applicant_experience}"
+        prompt = pre_prompt + f"The candidate's name is {applicant_fname} {applicant_lname}. \n"
+        prompt += f"The candidate's experience is: {applicant_experience} \n"
         # Job Specific Information
-        prompt += f"The date the application is being submitted is: {application_date}. "
-        prompt += f"The job title is: {job_title}. "
-        prompt += f"The job description is: {job_description}. "
-        prompt += f"The company's name is {company_name}. "
+        prompt += f"The date the application is being submitted is: {application_date}. \n"
+        prompt += f"The job title is: {job_title}. \n"
+        prompt += f"The job description is: {job_description}. \n"
+        prompt += f"The company's name is {company_name}. \n"
         if company_description!="Optional":
-            prompt += f"The company provided this information about themselves: {company_description}. "
-        
-        #TODO
+            prompt += f"The company provided this information about themselves: {company_description}. \n"
+
+        prompt += "Generate a cover letter at most 1000 words long using the above information."
+        #############################################
+        #TODO Get the experience from the resume pdf
         # prompt = f"The user's resume is {vector reference}" 
-        # text = st.text_area("Enter your text")
-        prompt += "Generate a cover letter at least 500 words long using the above information."
         # Create the vector reference for the resume
-        loader = PyPDFLoader(
-            file_path=user_info["resume_path"]
-            )
-        resume_data = loader.load()
-        text_chunk = RecursiveCharacterTextSplitter(chunk_size=10000, 
-                                                chunk_overlap=20).split_documents(resume_data)
-        # Setup the sentence transformer emb
-        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-        # Create an embedding store
-        vector_store = FAISS.from_documents(text_chunk, embedding=embeddings)
+        # loader = PyPDFLoader(
+        #     file_path=user_info["resume_path"]
+        #     )
+        # resume_data = loader.load()
+        # text_chunk = RecursiveCharacterTextSplitter(chunk_size=10000, 
+        #                                         chunk_overlap=20).split_documents(resume_data)
+        # # Setup the sentence transformer emb
+        # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+        # # Create an embedding store
+        # vector_store = FAISS.from_documents(text_chunk, embedding=embeddings)
+        #############################################
         model_info = json.load(open("user_info.json", encoding='utf-8'))['model_info']
         model_path = model_info["LLM_model_PATH"]
         llm = LlamaCpp(
-            streaming=True,
+            streaming=False,
             model_path=model_path,
-            temperature=0.7,
+            temperature=0.3,
             top_p=1,
-            n_batch=512,
-            max_new_tokens=2000,
-            n_ctx=8000,
-            verbose=True,
+            n_batch=128,
+            n_ctx=2000,
+            verbose=True
             )
+        # Write prompt to file
         with open('prompt.txt', 'w', encoding='utf-8') as f:
             f.write(prompt)
         prompt_template = PromptTemplate(
             input_variables=["prompt"],
             template="[INST] {prompt}[/INST]"
             )
-        qa = RetrievalQA.from_chain_type(
+        from langchain.chains import LLMChain
+        import langchain
+        # langchain.debug = True
+        llm = LLMChain(
             llm=llm,
-            retriever=vector_store.as_retriever(search_kwargs={'k': 1}),
-            chain_type="stuff"
-        )
-        qa_response = qa.run(query=prompt, return_text_length=2000)
+            prompt=prompt_template
+            )
+        llm_response = llm.run({"prompt":prompt, "max_tokens":200})
+        # llm_reply = llm_response({"prompt":prompt})
         with open('response.txt', 'w', encoding='utf-8') as f:
-            f.write(qa_response)
+            f.write(llm_response)
         st.subheader("Cover Letter:")
-        st.write(qa_response)
+        # st.write(qa_response)
+        st.write(llm_response)
         # TODO Add signature
         # if signature_path != None:
             # current_signature = open(signature_path, "r").read()
         # Write the cover letter to a file
         # TODO figure out how to write pdf or docx
         # print(prompt)
+        # TODO add dump to file button, so we can save the cover letter to a file to be used as 'good' examples for rag
 
         st.success('Done!')
 # %%
